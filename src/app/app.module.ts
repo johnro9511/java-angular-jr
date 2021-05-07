30e1aca4d8b649a982a90c880d7a55bf72631cea
johnro9511 <jmaldonadic@gmail.com>
2021-05-06 22:25:53 -0500

    APRENDIENDO ANGULAR-SPRING

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarComponent } from './Cliente/listar/listar.component';
+import { AddComponent } from './Cliente/add/add.component';
+import { EditComponent } from './Cliente/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarComponent,
+    AddComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
