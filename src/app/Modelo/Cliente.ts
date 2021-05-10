607726715b8dcfda04b7ab8bd0fc318beb046c93
johnro9511 <jmaldonadic@gmail.com>
2021-05-09 23:16:44 -0500

    AVANZANDO EN PROYECTO ANGULAR-SPRING
    AVANZANDO EN PROYECTO ANGULAR-SPRING

---
+++ src/app/Modelo/Cliente.ts
@@ -0,0 +1,7 @@
+export class Cliente{
+	rfc: String;
+	nom_cli: String;
+	direccion: String;
+	fecha_nac: String;
+	estado: number;
+}
--- src/app/Service/service.service.ts
+++ src/app/Service/service.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Cliente } from '../Modelo/Cliente';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
+	constructor(private http:HttpClient) {  }
 
-  constructor() { }
-}
+  	Url='http://localhost:8000/banco_jro/clientes';
+
+  	getClientes(){
+  		return this.http.get<Cliente[]>(this.Url);
+  	}//getClientes
+}//ServiceService
